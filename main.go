24591d3e7634a7bc9365eb082b58bbb9a738074b
Adhicitta Masran <adhicittamasran@gmail.com>
2023-03-07 16:22:22 +0800

    setup gorm

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"be-5/src/api/route"
+	"be-5/src/config/database"
 	"be-5/src/config/env"
 
 	"github.com/joho/godotenv"
@@ -12,6 +13,9 @@ func main() {
 	if err := godotenv.Load(); err != nil {
 		panic(err)
 	}
+
+	// migrate gorm
+	database.MigrateMySQL()
 
 	app := route.InitServer()
 	app.Logger.Fatal(app.Start(":" + env.GetServerEnv()))
---
+++ src/config/env/mysql.go
@@ -0,0 +1,7 @@
+package env
+
+import "os"
+
+func GetMySQLEnv() string {
+	return os.Getenv("MYSQL_DSN")
+}
