72868c63e4a5e87be9470927837c68f5d75728ee
Adhicitta Masran <adhicittamasran@gmail.com>
2023-03-10 23:08:24 +0800

    create fakultas handler

--- src/api/route/route.go
+++ src/api/route/route.go
@@ -1,6 +1,8 @@
 package route
 
 import (
+	"be-5/src/api/handler"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -12,6 +14,15 @@ func InitServer() *echo.Echo {
 	app.GET("", func(c echo.Context) error {
 		return c.JSON(200, "Welcome to IKU 5 API")
 	})
+
+	v1 := app.Group("/api/v1")
+
+	fakultas := v1.Group("/fakultas")
+	fakultas.GET("", handler.GetAllFakultasHandler)
+	fakultas.GET("/:id", handler.GetFakultasByIdHandler)
+	fakultas.POST("", handler.InsertFakultasHandler)
+	fakultas.PUT("/:id", handler.EditFakultasHandler)
+	fakultas.DELETE("/:id", handler.DeleteFakultasHandler)
 
 	return app
 }
