8d0e2ff549aa37fb8de49c370089e863ecc88629
Adhicitta Masran <adhicittamasran@gmail.com>
2023-03-23 21:49:52 +0800

    remove nidn field in admin response struct

--- src/api/response/admin.go
+++ src/api/response/admin.go
@@ -3,7 +3,6 @@ package response
 type Admin struct {
 	ID     int    `json:"id"`
 	Nama   string `json:"nama"`
-	Nidn   string `json:"nidn"`
 	Nip    string `json:"nip"`
 	Email  string `json:"email"`
 	Bagian string `json:"bagian"`
