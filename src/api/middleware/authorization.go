2f1a2274b19a6e977b0be22e953335d7b480529d
Adhicitta Masran <adhicittamasran@gmail.com>
2023-03-23 22:23:23 +0800

    add admin umum authorization middleware

--- src/api/middleware/authorization.go
+++ src/api/middleware/authorization.go
@@ -17,3 +17,15 @@ func GrantAdmin(next echo.HandlerFunc) echo.HandlerFunc {
 		return next(c)
 	}
 }
+
+func GrantAdminUmum(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		claims := util.GetClaimsFromContext(c)
+		if claims["role"].(string) != string(util.ADMIN) &&
+			claims["bagian"].(string) != "umum" {
+			return util.FailedResponse(c, http.StatusUnauthorized, nil)
+		}
+
+		return next(c)
+	}
+}
