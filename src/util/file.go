f1bf8a5317ac649b61e2cac9e5d9e28b7e83dc26
Adhicitta Masran <adhicittamasran@gmail.com>
2023-03-24 17:15:19 +0800

    add file type check util

---
+++ src/util/file.go
@@ -0,0 +1,27 @@
+package util
+
+import (
+	"errors"
+	"fmt"
+	"mime/multipart"
+)
+
+func CheckFileType(file *multipart.FileHeader) error {
+	fileType := file.Header["Content-Type"][0]
+	switch fileType {
+	case "application/pdf":
+		return nil
+	case "image/png":
+		return nil
+	case "image/jpeg":
+		return nil
+	case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+		return nil
+	case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+		return nil
+	case "text/plain":
+		return nil
+	}
+
+	return errors.New(fmt.Sprint("unsupported file type for ", file.Filename))
+}
