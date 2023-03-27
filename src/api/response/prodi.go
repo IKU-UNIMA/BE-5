688af69bb14f10c572ae062b7c20496ece737723
Adhicitta Masran <adhicittamasran@gmail.com>
2023-03-27 16:36:01 +0800

    add fakultas reference struct to custom the response

--- src/api/response/prodi.go
+++ src/api/response/prodi.go
@@ -1,9 +1,19 @@
 package response
 
 type Prodi struct {
-	ID         int      `json:"id"`
-	IdFakultas int      `json:"-"`
-	Nama       string   `json:"nama"`
-	Jenjang    string   `json:"jenjang"`
-	Fakultas   Fakultas `gorm:"foreignKey:IdFakultas" json:"fakultas"`
+	ID         int               `json:"id"`
+	IdFakultas int               `json:"-"`
+	KodeProdi  int               `json:"kode_prodi"`
+	Nama       string            `json:"nama"`
+	Jenjang    string            `json:"jenjang"`
+	Fakultas   FakultasReference `gorm:"foreignKey:IdFakultas" json:"fakultas"`
+}
+
+type FakultasReference struct {
+	ID   int    `json:"id"`
+	Nama string `json:"nama"`
+}
+
+func (FakultasReference) TableName() string {
+	return "fakultas"
 }
