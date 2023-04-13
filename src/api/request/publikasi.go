d9eedd31e2f98aa4d0e672d564bea9f7082f4b09
Adhicitta Masran <adhicittamasran@gmail.com>
2023-04-13 13:04:53 +0800

    add publikasi request and response structs

---
+++ src/api/request/publikasi.go
@@ -0,0 +1,83 @@
+package request
+
+import (
+	"be-5/src/model"
+	"be-5/src/util"
+	"errors"
+)
+
+type Publikasi struct {
+	IdKategori          int       `json:"id_kategori" validate:"required"`
+	IdJenisPenelitian   int       `json:"id_jenis" validate:"required"`
+	IdKategoriCapaian   int       `json:"id_kategori_capaian"`
+	Judul               string    `json:"judul" validate:"required"`
+	JudulAsli           string    `json:"judul_asli"`
+	JudulChapter        string    `json:"judul_chapter"`
+	NamaJurnal          string    `json:"nama_jurnal"`
+	NamaKoranMajalah    string    `json:"nama_koran_majalah"`
+	NamaSeminar         string    `json:"nama_seminar"`
+	TautanLamanJurnal   string    `json:"tautan_laman_jurnal"`
+	TanggalTerbit       string    `json:"tanggal_terbit" validate:"required"`
+	WaktuPelaksanaan    string    `json:"waktu_pelaksanaan"`
+	Volume              string    `json:"volume"`
+	Edisi               string    `json:"edisi"`
+	Nomor               string    `json:"nomor"`
+	Halaman             string    `json:"halaman"`
+	JumlahHalaman       int       `json:"jumlah_halaman"`
+	Penerbit            string    `json:"penerbit"`
+	KotaPenyelenggaraan string    `json:"kota_penyelenggaraan"`
+	IsSeminar           bool      `json:"is_seminar"`
+	IsProsiding         bool      `json:"is_prosiding"`
+	Bahasa              string    `json:"bahasa"`
+	Doi                 string    `json:"doi"`
+	Isbn                string    `json:"isbn"`
+	Issn                string    `json:"issn"`
+	EIssn               string    `json:"e_issn"`
+	Tautan              string    `json:"tautan"`
+	Keterangan          string    `json:"keterangan"`
+	Dokumen             []Dokumen `json:"dokumen"`
+	Penulis             []Penulis `json:"penulis"`
+}
+
+func (r *Publikasi) MapRequest() (*model.Publikasi, error) {
+	tanggalTerbit, err := util.ConvertStringToDate(r.TanggalTerbit)
+	if err != nil {
+		return nil, errors.New("format tanggal terbit salah")
+	}
+
+	waktuPelaksanaan, err := util.ConvertStringToDate(r.WaktuPelaksanaan)
+	if err != nil {
+		return nil, errors.New("format waktu pelaksanaan salah")
+	}
+
+	return &model.Publikasi{
+		IdKategori:          r.IdKategori,
+		IdJenisPenelitian:   r.IdJenisPenelitian,
+		IdKategoriCapaian:   r.IdKategoriCapaian,
+		Judul:               r.Judul,
+		JudulAsli:           r.JudulAsli,
+		JudulChapter:        r.JudulChapter,
+		NamaJurnal:          r.NamaJurnal,
+		NamaKoranMajalah:    r.NamaKoranMajalah,
+		NamaSeminar:         r.NamaSeminar,
+		TautanLamanJurnal:   r.TautanLamanJurnal,
+		TanggalTerbit:       tanggalTerbit,
+		WaktuPelaksanaan:    waktuPelaksanaan,
+		Volume:              r.Volume,
+		Edisi:               r.Edisi,
+		Nomor:               r.Nomor,
+		Halaman:             r.Halaman,
+		JumlahHalaman:       r.JumlahHalaman,
+		Penerbit:            r.Penerbit,
+		KotaPenyelenggaraan: r.KotaPenyelenggaraan,
+		IsSeminar:           r.IsSeminar,
+		IsProsiding:         r.IsProsiding,
+		Bahasa:              r.Bahasa,
+		Doi:                 r.Doi,
+		Isbn:                r.Isbn,
+		Issn:                r.Issn,
+		EIssn:               r.EIssn,
+		Tautan:              r.Tautan,
+		Keterangan:          r.Keterangan,
+	}, nil
+}
---
+++ src/api/response/publikasi.go
@@ -0,0 +1,87 @@
+package response
+
+type (
+	Publikasi struct {
+		ID                int               `json:"id"`
+		IdDosen           int               `json:"-"`
+		IdKategori        int               `json:"-"`
+		IdJenisPenelitian int               `json:"-"`
+		Judul             string            `json:"judul"`
+		TanggalTerbit     string            `json:"tanggal_terbit"`
+		Kategori          KategoriPublikasi `gorm:"foreignKey:IdKategori" json:"kategori_kegiatan"`
+		JenisPenelitian   JenisPenelitian   `gorm:"foreignKey:IdJenisPenelitian" json:"jenis"`
+	}
+
+	DetailPublikasi struct {
+		ID                  int                `json:"id"`
+		IdKategori          int                `json:"id_kategori"`
+		IdJenisPenelitian   int                `json:"-"`
+		IdKategoriCapaian   int                `json:"-"`
+		Judul               string             `json:"judul"`
+		JudulAsli           string             `json:"judul_asli"`
+		JudulChapter        string             `json:"judul_chapter"`
+		NamaJurnal          string             `json:"nama_jurnal"`
+		NamaKoranMajalah    string             `json:"nama_koran_majalah"`
+		NamaSeminar         string             `json:"nama_seminar"`
+		TautanLamanJurnal   string             `json:"tautan_laman_jurnal"`
+		TanggalTerbit       string             `json:"tanggal_terbit"`
+		WaktuPelaksanaan    string             `json:"waktu_pelaksanaan"`
+		Volume              string             `json:"volume"`
+		Edisi               string             `json:"edisi"`
+		Nomor               string             `json:"nomor"`
+		Halaman             string             `json:"halaman"`
+		JumlahHalaman       int                `json:"jumlah_halaman"`
+		Penerbit            string             `json:"penerbit"`
+		KotaPenyelenggaraan string             `json:"kota_penyelenggaraan"`
+		IsSeminar           bool               `json:"is_seminar"`
+		IsProsiding         bool               `json:"is_prosiding"`
+		Bahasa              string             `json:"bahasa"`
+		Doi                 string             `json:"doi"`
+		Isbn                string             `json:"isbn"`
+		Issn                string             `json:"issn"`
+		EIssn               string             `json:"e_issn"`
+		Tautan              string             `json:"tautan"`
+		Keterangan          string             `json:"keterangan"`
+		JenisPenelitian     JenisPenelitian    `gorm:"foreignKey:IdJenisPenelitian" json:"jenis_penelitian"`
+		KategoriCapaian     KategoriCapaian    `gorm:"foreignKey:IdKategoriCapaian" json:"kategori_capaian"`
+		PenulisDosen        []PenulisPublikasi `gorm:"foreignKey:IdPublikasi" json:"penulis_dosen"`
+		PenulisMahasiswa    []PenulisPublikasi `gorm:"foreignKey:IdPublikasi" json:"penulis_mahasiswa"`
+		PenulisLain         []PenulisPublikasi `gorm:"foreignKey:IdPublikasi" json:"penulis_lain"`
+		Dokumen             []DokumenPublikasi `gorm:"foreignKey:IdPublikasi" json:"dokumen"`
+	}
+
+	KategoriPublikasi struct {
+		ID                       int    `json:"id"`
+		IdJenisKategoriPublikasi int    `json:"-"`
+		Nama                     string `json:"nama"`
+	}
+
+	JenisKategoriPublikasi struct {
+		ID                int                 `json:"id"`
+		Nama              string              `json:"nama"`
+		KategoriPublikasi []KategoriPublikasi `gorm:"foreignKey:IdJenisKategoriPublikasi" json:"kategori_publikasi"`
+	}
+
+	PenulisPublikasi struct {
+		ID          int    `json:"id"`
+		IdPublikasi int    `json:"-"`
+		Nama        string `json:"nama"`
+		Urutan      int    `json:"urutan"`
+		Afiliasi    string `json:"afiliasi"`
+		Peran       string `json:"peran"`
+		IsAuthor    bool   `json:"is_author"`
+	}
+
+	DokumenPublikasi struct {
+		ID             string       `json:"id"`
+		IdPublikasi    int          `json:"-"`
+		IdJenisDokumen int          `json:"-"`
+		Nama           string       `json:"nama"`
+		NamaFile       string       `json:"nama_file"`
+		JenisFile      string       `json:"jenis_file"`
+		Keterangan     string       `json:"keterangan"`
+		Url            string       `json:"url"`
+		TanggalUpload  string       `json:"tanggal_upload"`
+		JenisDokumen   JenisDokumen `gorm:"foreignKey:IdJenisDokumen" json:"jenis_dokumen"`
+	}
+)
