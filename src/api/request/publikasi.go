00974817c67e77e4b86543b47f6e5676cfae004a
Adhicitta Masran <adhicittamasran@gmail.com>
2023-04-19 16:43:00 +0800

    publikasi: handle request if tanggal terbit or waktu pelaksanaan is empty

--- src/api/request/publikasi.go
+++ src/api/request/publikasi.go
@@ -4,6 +4,7 @@ import (
 	"be-5/src/model"
 	"be-5/src/util"
 	"errors"
+	"time"
 )
 
 type Publikasi struct {
@@ -43,14 +44,22 @@ type Publikasi struct {
 }
 
 func (r *Publikasi) MapRequest() (*model.Publikasi, error) {
-	tanggalTerbit, err := util.ConvertStringToDate(r.TanggalTerbit)
-	if err != nil {
-		return nil, errors.New("format tanggal terbit salah")
+	var (
+		tanggalTerbit, waktuPelaksanaan time.Time
+		err                             error
+	)
+	if r.TanggalTerbit != "" {
+		tanggalTerbit, err = util.ConvertStringToDate(r.TanggalTerbit)
+		if err != nil {
+			return nil, errors.New("format tanggal terbit salah")
+		}
 	}
 
-	waktuPelaksanaan, err := util.ConvertStringToDate(r.WaktuPelaksanaan)
-	if err != nil {
-		return nil, errors.New("format waktu pelaksanaan salah")
+	if r.WaktuPelaksanaan != "" {
+		waktuPelaksanaan, err = util.ConvertStringToDate(r.WaktuPelaksanaan)
+		if err != nil {
+			return nil, errors.New("format waktu pelaksanaan salah")
+		}
 	}
 
 	return &model.Publikasi{
