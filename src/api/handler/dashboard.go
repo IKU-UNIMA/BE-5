007cf9fd2ac8c2d89d48814a30d200b9063218c6
Adhicitta Masran <adhicittamasran@gmail.com>
2023-05-09 18:55:00 +0800

    dashboard: remove unused handler

--- src/api/handler/dashboard.go
+++ src/api/handler/dashboard.go
@@ -232,53 +232,6 @@ func GetDashboardByFakultasHandler(c echo.Context) error {
 	return util.SuccessResponse(c, http.StatusOK, data)
 }
 
-// func GetDetailDashboardHandler(c echo.Context) error {
-// 	fitur := c.Param("fitur")
-
-// 	if err := checkDashboardFitur(c, fitur); err != nil {
-// 		return err
-// 	}
-
-// 	queryParams := &dashboardQueryParam{}
-// 	if err := (&echo.DefaultBinder{}).BindQueryParams(c, queryParams); err != nil {
-// 		return util.FailedResponse(http.StatusBadRequest, map[string]string{"message": err.Error()})
-// 	}
-
-// 	db := database.InitMySQL()
-// 	ctx := c.Request().Context()
-// 	data := []response.DetailDashboard{}
-// 	condition := createTahunCondition(fitur, queryParams.Tahun)
-// 	if queryParams.Fakultas > 0 {
-// 		if condition != "" {
-// 			if fitur == "publikasi" {
-// 				fakultas := fmt.Sprintf(" AND dosen.id_fakultas = %d", queryParams.Fakultas)
-// 				splitTahun := strings.Split(condition, " OR ")
-// 				condition = splitTahun[0] + fakultas + " OR " + splitTahun[1] + fakultas
-// 			} else {
-// 				condition += fmt.Sprintf(" AND dosen.id_fakultas = %d", queryParams.Fakultas)
-// 			}
-// 		} else {
-// 			condition = fmt.Sprintf("WHERE dosen.id_fakultas = %d", queryParams.Fakultas)
-// 		}
-// 	}
-
-// 	query := fmt.Sprintf(
-// 		`SELECT prodi.id AS id_prodi, kode_prodi, prodi.nama AS nama_prodi, prodi.jenjang AS jenjang_prodi,
-// 			fakultas.id AS id_fakultas, fakultas.nama AS nama_fakultas, COUNT(*) AS jumlah FROM %s
-// 			JOIN dosen ON dosen.id = %s.id_dosen
-// 			JOIN prodi ON prodi.id = dosen.id_prodi
-// 			JOIN fakultas ON fakultas.id = dosen.id_fakultas
-// 			%s GROUP BY prodi.id ORDER BY prodi.id;`,
-// 		fitur, fitur, condition,
-// 	)
-
-// 	if err := db.WithContext(ctx).Raw(query).Find(&data).Error; err != nil {
-// 		return util.FailedResponse(http.StatusInternalServerError, nil)
-// 	}
-
-// 	return util.SuccessResponse(c, http.StatusOK, data)
-// }
-
 func GetDashboardTotalHandler(c echo.Context) error {
 	tahun := c.QueryParam("tahun")
 	db := database.InitMySQL()
