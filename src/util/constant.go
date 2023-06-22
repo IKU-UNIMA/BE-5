b32953f3219c0a93ee4b89d1ba74d3f5ac0d140d
Adhicitta Masran <adhicittamasran@gmail.com>
2023-06-22 17:54:28 +0800

    add status verifikasi data constants

--- src/util/constant.go
+++ src/util/constant.go
@@ -7,4 +7,10 @@ const (
 
 	// bagian admin
 	IKU5 = "IKU 5"
+
+	// status verifikasi data
+	BELUM_DIVERIFIKASI  = "Belum Diverifikasi"
+	DRAFT               = "Draft"
+	TIDAK_TERVERIFIKASI = "Tidak Terverifikasi"
+	TERVERIFIKASI       = "Terverifikasi"
 )
